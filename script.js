1275e6104bfb988416a7ae6b818db496a62ca05a
Seun Taiwo <seunemmanueltaiwo@gmail.com>
2022-09-09 02:53:09 +0100

    commit all

---
+++ script.js
@@ -0,0 +1,10 @@
+//Toggle Menu-Bar
+var navigateBar = document.getElementById('navLinks');
+
+		function showMenu() {
+			navigateBar.style.right = '0';
+		}
+
+		function closeMenu() {
+			navigateBar.style.right = '-150px';
+		}
